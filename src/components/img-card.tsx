5bfaf4113d65411859387cd824fa5156d5bc9590
zidan putra rahman <zidanputrarahman153@gmail.com>
2024-10-17 23:22:50 +0700

    initial assets

---
+++ src/components/img-card.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { ReactElement } from 'react'
+
+
+interface ImgCardProps{
+  img: ReactElement<typeof Image>;
+  title?: string;
+  description?: string;
+  rotate?: boolean;
+  showText?: boolean;
+}
+
+
+export function ImgCard({rotate = true, showText = false,...props}: ImgCardProps){
+  return (
+    <div className={`relative transition overflow-hidden rounded-xl group shadow-xl p-1 bg-white ${rotate ? "rotate-2 hover:rotate-0" : ""}`}>
+      {props.img}
+      <div className={`transition absolute bottom-0 right-0 left-0 p-2 bg-white/70 ${showText ? "" : "translate-y-36 group-hover:translate-y-0"}`}>
+        <h5 className='text-lg font-bold'>{props.title}</h5>
+        <p className='text-xs'>{props.description}</p>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,45 @@
+"use client"
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+
+
+interface NavbarProps {
+  backgroundShow?: boolean;
+}
+
+export function Navbar(){
+
+  const [showNavbarBackground, setShowNavbarBackGround] = useState(false);
+
+  function handleWindowScroll (){
+    if(!window) return;
+  
+    if(window.scrollY === 0) setShowNavbarBackGround(false);
+    else setShowNavbarBackGround(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleWindowScroll);
+    return () => window.removeEventListener("scroll", handleWindowScroll);
+  },[])
+
+  return (
+    <div className={clsx(
+      `md:px-36 `,
+      `fixed z-10 right-0 left-0 flex transition-all justify-between gap-2 text-white px-10 py-4 `,      
+      (showNavbarBackground ? "backdrop-filter backdrop-blur-lg bg-black bg-opacity-30" : "bg-green-600")
+      )
+    }>
+      <h1 className={"font-bold text-white text-xl  "}>Banyuwangi Supplier</h1>
+      <ul className="flex gap-5">
+        <li>
+          <a className="hover:underline underline-offset-2" href="https://wa.me/6281217692430">
+          Contact Us
+          </a>
+        </li>
+
+        {/* <li>Profile</li> */}
+      </ul>
+    </div>
+  )
+}
