8bc7d73886a4d551c006ccba4e45e55dd8451c34
zidan putra rahman <zidanputrarahman153@gmail.com>
2025-02-21 23:39:42 +0700

    update for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,17 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
+  viewport: { width: "device-width", initialScale: 1 },
   title: "Supplier Buah Lokal dan Impor ",
   description: "Menyediakan berbagai buah lokal dan impor",
-  
+  openGraph: {
+    type: "website",
+    title: "Supplier Buah Lokal dan Impor ",
+    description: "supplier buah lokal dan impor",
+  },
+  keywords: [
+    "buah", "lokal", "impor", "banyuwangi", "local", "indonesia", "terpercaya"
+  ]
 };
 
 export default function RootLayout({
